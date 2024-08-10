432cd3a1afa6f644255352d57602798bca666200
yeowon <dudnjsckrgo@gmail.com>
2024-08-10 18:19:48 +0900

    design: 높이 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = ({ title }: { title: string }) => (
 	<div className="sticky bg-white/75 z-10 backdrop-blur-md top-0">
-		<div className="flex items-center justify-between px-4 py-3">
+		<div className="flex items-center justify-between px-4 py-2">
 			<div>
 				{/* <Text variant='large/bold'>Home</Text> */}
 				<h2 className="text-lg font-bold">{title}</h2>
