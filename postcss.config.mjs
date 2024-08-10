f184abb7a6075056e5bdc9e4e51fb9783939fdd6
yeowon <dudnjsckrgo@gmail.com>
2024-08-10 18:21:28 +0900

    enhance: postcss cssnano 압축 적용  css 최적화

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    'postcss-import': {}, // PostCSS Import 플러그인
+    'tailwindcss': {}, // TailwindCSS 플러그인
+    'autoprefixer': {}, // Autoprefixer 플러그인
+    'cssnano': process.env.NODE_ENV === 'production' ? {} : false, // 프로덕션 빌드 시에만 cssnano 사용
   },
 };
 
