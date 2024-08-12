c23d47c4974abc247db6b0851eb36eeecb9b019c
윤여원 <dudnjsckrgo@gmail.com>
2024-08-12 11:10:48 +0900

    feat: toggle 기능

--- components/PanelItem.tsx
+++ components/PanelItem.tsx
@@ -1,28 +1,50 @@
+"use client";
+import React, { useState } from 'react';
 import Avatar from '@rd/Avatar';
-import Button from '@ui/Button';
+import { Button } from '@nextui-org/button';
+import UserInfo from '@components/UserInfo';
+import TierBadge from '@components/TierBadge';
 
 interface Props {
-	name: string;
-	username: string;
-	src: string;
-	initials: string;
+  name: string;
+  username: string;
+  src: string;
+  initials: string;
 }
 
-const PanelItem = ({ name, username, src, initials }: Props) => (
-	<div className="flex flex-1 items-center gap-x-2 px-4 py-3 hover:bg-slate-200">
-		<div className="flex items-center gap-x-2 flex-1">
-			<div className="flex flex-1 xl:flex-none justify-center xl:justify-start">
-				<Avatar src={src} alt={name} initials={initials} />
-			</div>
-			<div className="hidden xl:flex flex-col ">
-				<p className="text-base font-semibold">{name}</p>
-				<p className="text-sm text-slate-600 font-medium">@{username}</p>
-			</div>
-		</div>
-		<div className="">
-			<Button size="small">Follow</Button>
-		</div>
-	</div>
-);
+const PanelItem = ({ name, username, src, initials }: Props) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const handleToggle = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  return (
+    <div className="flex flex-1 items-center gap-x-2 px-4 py-3 hover:bg-slate-200">
+      <div className="flex items-center gap-x-2 flex-1">
+        <div className="flex-none justify-start">
+          <Avatar src={src} alt={name} initials={initials} />
+        </div>
+        <div className=" flex flex-col ">
+          <TierBadge name="nostradamus" />
+          <p className="text-base font-semibold">{name}</p>
+          <p className="text-sm text-slate-600 font-medium">@{username}</p>
+        </div>
+      </div>
+      <div>
+        <Button
+          radius="full"
+          size="md"
+          className="px-4 py-2"
+          variant={isFollowing ? 'bordered' : 'solid'}
+          color="primary"
+          onClick={handleToggle}
+        >
+          {isFollowing ? 'Following' : 'Follow'}
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default PanelItem;
