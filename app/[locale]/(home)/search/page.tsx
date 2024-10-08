61abd9f0c210a59e1b920629a0013eae10a311fa
윤여원 <dudnjsckrgo@gmail.com>
2024-10-08 09:07:22 +0900

    feat: 무한 스크롤

--- app/[locale]/(home)/search/page.tsx
+++ app/[locale]/(home)/search/page.tsx
@@ -1,22 +1,28 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import StickyTabsWrapper from '@components/StickyTabs';
 import Search from '@ui/CustomSearch';
 import CategoryChannelCard from '@ui/CategoryChannelCard';
 import Header from '@ui/Header';
 import Account from '@ui/Account';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { debounce } from 'lodash'; // lodash를 사용하여 debounce 적용
 import apiClient from '@api/axios'
 import Link from 'next/link';
 import { useDebounce } from "@uidotdev/usehooks";
+import { useInView } from 'react-intersection-observer';
 
-// API 호출 함수
-const fetchSearchResults = async (queryKey: string) => {
-  const response = await apiClient.get(`/search/displayName?q=${queryKey}`);
+
+// Fetch search results
+const fetchSearchResults = async (pageParam:number ,queryKey: string[]) => {
+  const response = await apiClient.get(`/search/user/displayName`,{
+    params: { 
+      q: queryKey[1],
+      page: pageParam,
+      size: 5,
+      }} );
   return response.data;
 };
-
 export default function Page() {
   const tabs = ["내 관심", "탐색"];
   const [activeTab, setActiveTab] = useState(0); // 탭 상태 관리
@@ -24,6 +30,10 @@ export default function Page() {
   const [isSearching, setIsSearching] = useState(false); // 검색 상태 관리
   const [searchQuery, setSearchQuery] = useState(''); // 검색 쿼리 상태
   const debouncedSearchTerm = useDebounce(searchQuery, 500);
+  const { ref: loadMoreRef, inView: isInView } = useInView({
+    threshold: 0.5, // 50%가 보일 때 트리거
+    triggerOnce: false, // 여러 번 트리거되도록 설정
+  });
 
   // 검색 상태와 헤더 상태 토글 함수
   const handleSearchToggle = () => {
@@ -35,19 +45,47 @@ export default function Page() {
   const handleSearchInput = debounce((input: string) => {
     setSearchQuery(input);
   }, 300);
+  const {
+    fetchNextPage: fetchNextPageSearch,
+    fetchPreviousPage: fetchPreviousPageSearch,
+    hasNextPage : hasNextPageSearch,
+    hasPreviousPage:hasPreviousPageSearch,
+    isFetchingNextPage: isFetchingNextPageSearch,
+    isFetchingPreviousPage:isFetchingPreviousPageSearch,
+    data: search,
+    error:infiniteErrorSearch,
+    status:statusSearch
 
-  // React Query로 검색 API 호출
-  const { data: searchResults, isLoading, error } = useQuery({
-    queryKey: ['searchResults', debouncedSearchTerm], // queryKey로 검색 쿼리 관리
-    queryFn: () => fetchSearchResults(debouncedSearchTerm), // 검색 API 호출 함수
-    enabled: !!debouncedSearchTerm, // searchQuery가 존재할 때만 요청 수행
-    staleTime: 5 * 60 * 1000, // 5분 동안 캐시 유지
-  });
+  } = useInfiniteQuery(
+    {
+      queryKey: ['searchResults',debouncedSearchTerm],
+      queryFn: ({ pageParam = 1 ,queryKey })=> fetchSearchResults(pageParam,queryKey),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPages) => {
+      // lastPage는 Spring Page 객체입니다.
+      console.log(lastPage);
+      if (!lastPage.last) {
+        return lastPage.number + 1; // 다음 페이지 번호를 반환
+      } else {
+        return undefined; // 더 이상 페이지가 없으면 undefined 반환
+      }
+      },
+      enabled: !!debouncedSearchTerm,
+      staleTime: 5 * 60 * 1000,
+    }
+  );
 
   // 탭 변경 핸들러
   const handleTabChange = (index: number) => {
     setActiveTab(index);
   };
+  useEffect(() => {
+    console.log("hasNextPage",hasNextPageSearch)
+    console.log("isInView",isInView)
+    if (isInView && hasNextPageSearch) {
+      fetchNextPageSearch();
+    }
+  }, [isInView, hasNextPageSearch, fetchNextPageSearch]);
 
   return (
     <>
@@ -131,23 +169,39 @@ export default function Page() {
         {/* 검색 중일 때 검색 결과 표시 */}
         {isSearching && (
           <div className="p-4">
-            {isLoading ? (
-              <p>검색 중...</p>
-            ) : error ? (
-              <p>에러 발생: {error.message}</p>
-            ) : (
-              searchResults?.content.map((result: any) => (
-                <Link key={result.userId} href={`/channel/${result.user_id}`}>
-                  <Account
-                    className="px-2 py-2"
-                    userName={result.user_name}
-                    avatarUrl={result.avatar_img}
-                    displayName={result.display_name}
-                    tier={result.tier}
-                  />
-                </Link>
-              ))
-            )}
+        
+            
+{statusSearch === 'pending' ? (
+                    <p>Loading categories...</p>
+                  ) : statusSearch === 'error' ?  <p>Error: {infiniteErrorSearch.message}</p>: 
+                 (
+
+
+                  search?.pages.map((page, pageIndex) =>(
+                    <React.Fragment key={pageIndex}>
+                      {page.content.map( ((result: any) => (
+                 <Link key={result.userId} href={`/channel/${result.user_id}`}>
+                 <Account
+                   className="px-2 py-2"
+                   userName={result.user_name}
+                   avatarUrl={result.avatar_img}
+                   displayName={result.display_name}
+                   tier={result.tier}
+                 />
+               </Link>
+                 ))
+                      )
+                      }
+                    </React.Fragment>
+                  ))
+                   
+                  )}
+                   <div ref={loadMoreRef}>
+                
+                {isFetchingNextPageSearch ? 'Loading more...' : hasNextPageSearch
+                     ? 'Load More'
+                     : ''}
+              </div>
           </div>
         )}
       </main>
