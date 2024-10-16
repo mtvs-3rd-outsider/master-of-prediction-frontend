6f02976e5cad6c56208fb2df697f5ef8bf1406ed
윤여원 <dudnjsckrgo@gmail.com>
2024-10-16 15:26:49 +0900

    fix: build error not-found dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import type { Metadata, Viewport } from "next";
 import Head from 'next/head';
 import {NextIntlClientProvider} from 'next-intl';
-import { getMessages } from "next-intl/server";
+import { getMessages, unstable_setRequestLocale } from "next-intl/server";
 import { Toaster } from "react-hot-toast";
 const APP_NAME = "PWA App";
   const APP_DEFAULT_TITLE = "My Awesome PWA App";
@@ -48,7 +48,11 @@ const APP_NAME = "PWA App";
   export const viewport: Viewport = {
     themeColor: "#FFFFFF",
   };
- 
+  export async function generateStaticParams() {
+    unstable_setRequestLocale('ko'); // 기본 로케일을 설정하세요.
+    return []; // 추가적인 정적 매개변수가 있으면 반환하세요.
+  }
+  
 export default async  function RootLayout({
   children,
   params: {locale}
---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+// app/not-found.tsx
+
+export const dynamic = 'force-dynamic';
+
+export default function NotFound() {
+  return <h1>페이지를 찾을 수 없습니다.</h1>;
+}
