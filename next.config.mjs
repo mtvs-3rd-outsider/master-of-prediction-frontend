6d911b6a04a3b20d7ad0b9682d45a0f973bdeecc
apfp77 <apfp77@naver.com>
2024-10-25 20:06:23 +0900

    feat: 이미지 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
 } from "next/constants.js";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 const require = createRequire(import.meta.url);
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
@@ -12,62 +12,67 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withNextIntl(withBundleAnalyzer({
-  compress: true,
-  reactStrictMode: false,
-      // styled-components 설정 추가
-      compiler: {
-        styledComponents: true,
-        // removeConsole: true,
-        
-      },
-  images: {
-    formats: ["image/webp", "image/avif"], // webp와 avif 포맷 추가
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: 'http',
-        hostname: '125.132.216.190',
-      },
-    ],
-    domains: ["localhost"], // localhost 도메인 추가
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!isServer) {
-      config.optimization.minimize = true;
-      config.optimization.splitChunks = {
-        chunks: "all",
-        minSize: 0,
-        maxSize: 200000, // 200KB
-      };
-    }
-    if (!dev && !isServer) {
-      // 프로덕션 클라이언트 빌드에서 stories 파일 제외
-      config.module.rules.push({
-        test: /\.stories\.tsx?$/,
-        loader: "ignore-loader",
-      });
-    }
-    return config;
-  },
-
-
-}));
+const nextConfig = withNextIntl(
+  withBundleAnalyzer({
+    compress: true,
+    reactStrictMode: false,
+    // styled-components 설정 추가
+    compiler: {
+      styledComponents: true,
+      // removeConsole: true,
+    },
+    images: {
+      formats: ["image/webp", "image/avif"], // webp와 avif 포맷 추가
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "pbs.twimg.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "sports-phinf.pstatic.net",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "via.placeholder.com",
+        },
+        {
+          protocol: "http",
+          hostname: "125.132.216.190",
+        },
+      ],
+      domains: ["localhost"], // localhost 도메인 추가
+    },
+    webpack: (config, { dev, isServer }) => {
+      if (!isServer) {
+        config.optimization.minimize = true;
+        config.optimization.splitChunks = {
+          chunks: "all",
+          minSize: 0,
+          maxSize: 200000, // 200KB
+        };
+      }
+      if (!dev && !isServer) {
+        // 프로덕션 클라이언트 빌드에서 stories 파일 제외
+        config.module.rules.push({
+          test: /\.stories\.tsx?$/,
+          loader: "ignore-loader",
+        });
+      }
+      return config;
+    },
+  })
+);
 const nextConfigFunction = async (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     const withPWA = (await import("@ducanh2912/next-pwa")).default({
