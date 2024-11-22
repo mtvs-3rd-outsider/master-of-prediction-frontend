e9265fd343455b41dc29cc01a339f347d80cd590
윤여원 <dudnjsckrgo@gmail.com>
2024-11-23 00:10:00 +0900

    채널 패치를 못 받으면 로그인으로 가도록

--- app/[locale]/(home)/channel/[userId]/page.tsx
+++ app/[locale]/(home)/channel/[userId]/page.tsx
@@ -1,11 +1,22 @@
 // app/page.tsx (서버 컴포넌트)
-import React from 'react';
-import UserChannelPageClientComponent from '@components/UserChannelPageClientComponent';
-import fetchWithBaseURL from '@api/fetch'; 
+import React from "react";
+import { redirect } from "next/navigation"; // Next.js 리디렉션 유틸
+import UserChannelPageClientComponent from "@components/UserChannelPageClientComponent";
+import fetchWithBaseURL from "@api/fetch";
+import useUserStore from "@store/useUserStore"; // Zustand 스토어 사용
 
 // 데이터 페칭 함수: userId를 사용하여 API 요청
 async function fetchUserData(userId: string): Promise<MyChannelProps> {
-  return fetchWithBaseURL(`/my-channel/${userId}`);
+  try {
+    return await fetchWithBaseURL(`/my-channel/${userId}`);
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    // Zustand의 clearUserInfo 메서드 호출
+    const clearUserInfo = useUserStore.getState().clearUserInfo;
+    clearUserInfo(); // 상태 초기화
+    redirect("/login"); // 로그인 페이지로 리디렉션
+    throw error; // 오류를 다시 던져서 이후 동작 방지
+  }
 }
 
 // 구조분해를 사용한 MyChannelProps 타입
@@ -19,29 +30,26 @@ export type MyChannelProps = {
   location: string;
   website: string;
   birthdate: string;
-  joined_date: string;   // 변경됨
+  joined_date: string; // 변경됨
   user_gender: string;
   user_point: number;
   transactions: number;
-  profit_rate: string;   // 변경됨
+  profit_rate: string; // 변경됨
   position_value: string; // 변경됨
-  trade_count: number;   // 변경됨
+  trade_count: number; // 변경됨
   following_count: number; // 변경됨
-  follower_count: number;  // 변경됨
-  tier_name:string;
-  tier_level:number;
+  follower_count: number; // 변경됨
+  tier_name: string;
+  tier_level: number;
 };
 
 export default async function Page({ params }: { params: { userId: string } }) {
-  
   // 서버에서 데이터 페칭
   const userData = await fetchUserData(params.userId);
   const updatedData = {
     ...userData,
     userId: params.userId, // userId를 추가
   };
-
-  // 탭 데이터
 
   return (
     <>
